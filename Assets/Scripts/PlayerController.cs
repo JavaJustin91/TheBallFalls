34914c4c182605821284496f8c69ab49c6388672
Justin Jones <javajustin91@gmail.com>
2024-02-24 15:13:29 -0500

    starting player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log(horizontalInput);
+        //rb.AddForce(Vector3.forward * horizontalInput * Time.deltaTime * speed, ForceMode.Impulse);
     }
 }
